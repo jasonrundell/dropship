28fc8f118d3ead86f4286ed1f50431271f7094c0
Jason Rundell <jason.rundell@gmail.com>
2022-09-15 10:13:16 -0400

    Intro update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/Intro.stories.mdx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: [
     {
       name: '@storybook/addon-docs',
