4d8b9e5e2da220427dc77e504e4f3caae2dcca67
Jason Rundell <jason.rundell@gmail.com>
2023-01-03 23:49:46 -0500

    Link component

---
+++ src/stories/Link.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './link.css'
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Link = ({ primary, to, size, label, target, rel, ...props }) => {
+  const mode = primary
+    ? 'storybook-link--primary'
+    : 'storybook-link--secondary'
+  return (
+    <a
+      href={to}
+      className={['storybook-link', `storybook-link--${size}`, mode].join(
+        ' '
+      )}
+      {...props}
+      target={target}
+      rel={rel}
+    >
+      {label}
+    </a>
+  )
+}
+
+Link.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What url to link to
+   */
+  to: PropTypes.string,
+  /**
+   * How large should the link be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Link contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+  /**
+   * Target for the link
+   */
+  target: PropTypes.string,
+  /**
+   * Rel for the link
+  */
+  rel: PropTypes.string
+}
+
+Link.defaultProps = {
+  primary: false,
+  to: '#',
+  size: 'medium',
+  onClick: undefined,
+  target: null,
+  rel: null
+}
---
+++ src/stories/Link.stories.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { userEvent, within } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+
+import { Link } from './Link'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Atoms/Link',
+  component: Link
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Link {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Link'
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  label: 'Link'
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+  label: 'Link'
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  label: 'Link'
+}
+
+export const WithInteractions = (args) => <Link {...args} />
+WithInteractions.args = {
+  primary: true,
+  to: 'https://github.com/jasonrundell/dropship',
+  size: 'medium',
+  onClick: undefined,
+  label: 'Dropship',
+  target: '_blank',
+  rel: 'noopener noreferrer'
+}
+
+WithInteractions.play = async ({ canvasElement }) => {
+  // Assigns canvas to the component root element
+  const canvas = within(canvasElement)
+  const linkElement = canvas.getByRole('a')
+  await userEvent.click(linkElement)
+  expect(linkElement).not.toBeNull()
+  expect(linkElement).toHaveAttribute('type', 'button')
+  expect(linkElement.textContent).toEqual('DROPSHIP')
+}
