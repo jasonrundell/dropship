3f0c512a72b3779ba42df7e857e024cebddda038
Jason Rundell <jason.rundell@gmail.com>
2023-01-16 16:47:23 -0500

    Paragraph component

---
+++ src/stories/atoms/Paragraph/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './paragraph.css'
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Paragraph = ({ children }) => (
+  <p className="storybook-p">{children}</p>
+)
+
+Paragraph.propTypes = {
+  /**
+   * Paragraph content
+   */
+  children: PropTypes.any.isRequired
+}
+
+Paragraph.defaultProps = {}
