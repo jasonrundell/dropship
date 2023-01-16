98ce662acde287ef868b0dd62cc7cee89277d058
Jason Rundell <jason.rundell@gmail.com>
2023-01-16 00:04:35 -0500

    Box component added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as icons from './shared/icons'
 export { icons }
 
 export * from './Avatar'
+export * from './Box'
 export * from './Button'
 export * from './Heading'
 export * from './Icon'
---
+++ src/stories/Box.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { classNames } from '../utils/css'
+import './box.css'
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Box = ({ isTight, isRoomy, children }) => {
+  const classes = classNames(
+    'storybook-box',
+    isTight && 'storybook-box--tight',
+    isRoomy && 'storybook-box--roomy'
+  )
+  return <div className={classes}>{children}</div>
+}
+
+Box.propTypes = {
+  /**
+   * Smaller padding
+   */
+  isTight: PropTypes.bool,
+  /**
+   * Larger padding
+   */
+  isRoomy: PropTypes.bool,
+  /**
+   * Box can contain any elements
+   */
+  children: PropTypes.any.isRequired
+}
+
+Box.defaultProps = {
+  isTight: false,
+  isRoomy: false
+}
