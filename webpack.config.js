f086d471a14fcfcc894b6340cd782c919a7e8a29
Jason Rundell <jason.rundell@gmail.com>
2023-02-20 23:55:36 -0500

    Name change

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    library: 'Dropship',
+    library: '@jasonrundell/dropship',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
