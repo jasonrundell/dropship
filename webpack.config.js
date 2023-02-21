b0498443db7c7188281a886e727349acc34251b8
Jason Rundell <jason.rundell@gmail.com>
2023-02-21 11:58:04 -0500

    Build updates

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: '@jasonrundell/dropship',
-    libraryTarget: 'commonjs'
+    filename: 'index.js'
   },
   resolve: {
     extensions: ['.js', '.jsx']
