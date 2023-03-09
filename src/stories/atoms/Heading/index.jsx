13eccc1651700a59e0090ff0ef9d268adf368579
Jason Rundell <jason.rundell@gmail.com>
2023-03-09 10:29:11 -0500

    Emoition-ize Heading Component

--- src/stories/atoms/Heading/index.jsx
+++ src/stories/atoms/Heading/index.jsx
@@ -1,40 +1,93 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './index.module.scss'
+import { css } from '@emotion/react'
 
-export const Heading = ({ level, label }) => {
+const styleH1 = css`
+  font-size: 2.5rem;
+  line-height: 2.5rem;
+  margin-top: 0;
+  margin-bottom: 2.5rem;
+`
+
+const styleH2 = css`
+  font-size: 1.5rem;
+  line-height: 1.5rem;
+  margin-top: 0;
+  margin-bottom: 1.5rem;
+`
+
+const styleH3 = css`
+  font-size: 1.25rem;
+  line-height: 1.25rem;
+  margin-top: 0;
+  margin-bottom: 1.25rem;
+`
+const styleH4 = css`
+  font-size: 1rem;
+  line-height: 1rem;
+  margin-top: 0;
+  margin-bottom: 1rem;
+`
+
+const styleH5 = css`
+  font-size: 0.875rem;
+  line-height: 0.875rem;
+  margin-top: 0;
+  margin-bottom: 0.875rem;
+`
+
+const styleH6 = css`
+  font-size: 0.75rem;
+  line-height: 0.75rem;
+  margin-top: 0;
+  margin-bottom: 0.75rem;
+`
+
+export const Heading = ({ level, label, classNames }) => {
   let titleClass
   let Component
+  let style
   switch (level) {
     case 1:
       titleClass = 'h1'
       Component = 'h1'
+      style = styleH1
       break
     case 2:
       titleClass = 'h2'
       Component = 'h2'
+      style = styleH2
       break
     case 3:
       titleClass = 'h3'
       Component = 'h3'
+      style = styleH3
       break
     case 4:
       titleClass = 'h4'
       Component = 'h4'
+      style = styleH4
       break
     case 5:
       titleClass = 'h5'
       Component = 'h5'
+      style = styleH5
       break
     case 6:
       titleClass = 'h6'
       Component = 'h6'
+      style = styleH6
       break
     default:
       titleClass = 'h1'
       Component = 'h1'
+      style = styleH1
   }
-  return <Component className={styles[titleClass]}>{label}</Component>
+  return (
+    <Component css={style} className={classNames}>
+      {label}
+    </Component>
+  )
 }
 
 Heading.propTypes = {
@@ -45,7 +98,11 @@ Heading.propTypes = {
   /**
    * Heading text.
    */
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  /**
+   * Assign a custom class name or multiple class names to the section.
+   */
+  classNames: PropTypes.string
 }
 
 Heading.defaultProps = {
