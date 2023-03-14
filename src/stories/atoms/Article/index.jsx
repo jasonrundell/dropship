57ebb311df1f45345053673d5b863b521b5aa7b1
Jason Rundell <jason.rundell@gmail.com>
2023-03-14 13:33:31 -0400

    Article component

---
+++ src/stories/atoms/Article/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { css } from '@emotion/react'
+
+export const Article = ({ id, classNames, children }) => {
+  const style = css`
+    width: 100%;
+  `
+
+  return (
+    <article id={id} css={style} className={classNames}>
+      {children}
+    </article>
+  )
+}
+
+Article.propTypes = {
+  /**
+   * Assign a unique id to the section. A common use-case.
+   */
+  id: PropTypes.string,
+  /**
+   * Assign a custom class name or multiple class names to the component.
+   */
+  classNames: PropTypes.string,
+  /**
+   * Section content.
+   */
+  children: PropTypes.any.isRequired
+}
+
+Article.defaultProps = {
+  id: null,
+  classNames: null
+}
