aade5316068e52a2d36f839ec2939c5467ef1107
Jason Rundell <jason.rundell@gmail.com>
2023-09-27 15:49:04 -0400

    Adding missing index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as icons from './shared/icons'
+
+export { icons }
+
+export * from './stories/atoms'
+// export * from './stories/molecules' // disable while none exist
+export * from './stories/organisms'
