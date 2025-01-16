367bb76d6c17b5a52416d700c1c5f69d164912cf
Jason Rundell <jason.rundell@gmail.com>
2025-01-16 02:02:10 -0500

    Refactor Container component styles to use object syntax for improved readability and maintainability

--- src/stories/atoms/Container/Container.tsx
+++ src/stories/atoms/Container/Container.tsx
@@ -7,14 +7,13 @@ interface ContainerProps {
   children: React.ReactNode
 }
 
-const StyledContainer = styled('div')<ContainerProps>`
-  margin: 0 auto;
-  padding: 0 2rem;
-
-  @media (min-width: ${Tokens.sizes.breakpoints.medium}rem) {
-    max-width: ${Tokens.sizes.breakpoints.medium}rem;
+const StyledContainer = styled('div')<ContainerProps>({
+  margin: '0 auto',
+  padding: `0 ${Tokens.sizes.padding.large}rem`,
+  '@media (min-width: 48rem)': {
+    maxWidth: `${Tokens.sizes.breakpoints.medium}rem`
   }
-`
+})
 
 const Container = ({ children, ...props }: ContainerProps) => {
   return <StyledContainer {...props}>{children}</StyledContainer>
